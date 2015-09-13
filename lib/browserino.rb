e85b6cee96815aa5f2996e910b618da27dd6043e
Sidney Liebrand <sidneyliebrand@gmail.com>
2015-09-13 05:34:16 +0200

    added regex patterns for major browsers

--- lib/browserino.rb
+++ lib/browserino.rb
@@ -1,8 +1,10 @@
 require "browserino/version"
+require "browserino/patterns"
 require "browserino/browser"
 require "browserino/engine"
 require "browserino/operating_system"
 
 module Browserino
-
+  def parse ua
+  end
 end
---
+++ lib/browserino/patterns.rb
@@ -0,0 +1,53 @@
+# See this as a giant CSS selector.
+# It will iterate through hashes recursively.
+# If the :name is set in e.g. :browser then it will be global for all browsers.
+# However if in :browser there is a hash called :opera with a :name
+# then that will override the global setting.
+# This basically follows the 'Cascading Style Sheet' flow
+
+module Browserino
+  PATTERNS = {
+    browser: {
+      opera: {
+        name: /ope?ra?/,
+        version: /(ope?ra?|version)(\/|\s)([\d\.]+)/
+      },
+
+      ie: {
+        name: /msie|trident/,
+        version: /(msie\s|rv\:)([\d\.b]+)/
+      },
+
+      maxthon: {
+        name: /maxthon/,
+        version: /maxthon(\s|\/)([\d\.]+)/
+      },
+
+      firefox: {
+        name: /(fire|water)(fox|bird)|ice(weasel|cat)|netscape/,
+        version: /(fire|water)(fox|bird)|ice(weasel|cat)|netscape([\d\.]+)/
+      }
+
+      chrome: {
+        name: /chrome?(ium|plus)?/,
+        version: /chrome?(ium|plus)?\/([\d\.]+)/
+      },
+
+      safari: {
+        name: /safari/,
+        version: /(version|safari)\/([\d\.]+)/
+      }
+    },
+
+    engine: {
+      name: /((apple)?webkit|gecko|trident|presto)/,
+      version: /((apple)?webkit|gecko|trident|presto)([\d\.]+)/
+    },
+
+    operating_system: {
+      name: /windows|macintosh|linux/,
+      version: /(nt|mac\sos\sx)\s([\d\._]+)/
+      architecture: /(x?(86_)?64)|i(3|6)86/
+    }
+  }
+end
