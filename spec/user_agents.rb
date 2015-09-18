14b957b472b394d5815ded5513b1e2a005b2c702
Sidney Liebrand <sidneyliebrand@gmail.com>
2015-09-18 16:53:29 +0200

    modified tests for aliassed output

--- spec/user_agents.rb
+++ spec/user_agents.rb
@@ -92,7 +92,7 @@ module UserAgents
       'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.1 (KHTML, like Gecko) Maxthon/3.0.8.2 Safari/533.1' => {
         browser_name: 'Maxthon',
         browser_version: '3.0.8.2',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '533.1',
         system_name: 'Windows',
         system_version: '6.0'
@@ -169,7 +169,7 @@ module UserAgents
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36' => {
         browser_name: 'Chrome',
         browser_version: '37.0.2062.124',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.36',
         system_name: 'Macintosh',
         system_version: '10_10_1'
@@ -177,7 +177,7 @@ module UserAgents
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36' => {
         browser_name: 'Chrome',
         browser_version: '36.0.1944.0',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.36',
         system_name: 'Macintosh',
         system_version: '10_9_2'
@@ -187,7 +187,7 @@ module UserAgents
       'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36' => {
         browser_name: 'Chrome',
         browser_version: '41.0.2228.0',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.36',
         system_name: 'Windows',
         system_version: '6.1'
@@ -195,7 +195,7 @@ module UserAgents
       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36' => {
         browser_name: 'Chrome',
         browser_version: '31.0.1623.0',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.36',
         system_name: 'Windows',
         system_version: '6.1'
@@ -205,7 +205,7 @@ module UserAgents
       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F' => {
         browser_name: 'Chrome',
         browser_version: '34.0.1847.137',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.36',
         system_name: 'unknown',
         system_version: 'unknown'
@@ -213,7 +213,7 @@ module UserAgents
       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36' => {
         browser_name: 'Chrome',
         browser_version: '41.0.2227.0',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.36',
         system_name: 'unknown',
         system_version: 'unknown'
@@ -225,7 +225,7 @@ module UserAgents
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A' => {
         browser_name: 'Safari',
         browser_version: '7.0.3',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.75.14',
         system_name: 'Macintosh',
         system_version: '10_9_3'
@@ -233,7 +233,7 @@ module UserAgents
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2' => {
         browser_name: 'Safari',
         browser_version: '5.1.7',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '537.13',
         system_name: 'Macintosh',
         system_version: '10_6_8'
@@ -243,7 +243,7 @@ module UserAgents
       'Mozilla/5.0 (Windows; U; Windows NT 6.1; tr-TR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27' => {
         browser_name: 'Safari',
         browser_version: '5.0.4',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '533.20.25',
         system_name: 'Windows',
         system_version: '6.1'
@@ -251,7 +251,7 @@ module UserAgents
       'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0 Safari/533.16' => {
         browser_name: 'Safari',
         browser_version: '5.0',
-        engine_name: 'AppleWebkit',
+        engine_name: 'webkit',
         engine_version: '533.18.1',
         system_name: 'Windows',
         system_version: '6.1'
