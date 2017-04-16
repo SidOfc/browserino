fe131dec5b0e16d2d752ae2ec160d99165103367
Sidney Liebrand <sidneyliebrand@gmail.com>
2017-04-16 02:38:50 +0200

    added name tests, cleaned up browser spec test

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -9,17 +9,24 @@ describe 'Browserino' do
     describe spec[:user_agent] do
       spec.reject { |k| k == :user_agent }.each do |test_method, test_result|
         it "correctly outputs #{test_method} for #{agent.name}" do
-          if /version/i.match?(test_method.to_s)
-            test_result = Browserino::Agent::Version.new test_result
-          end
-
           expect(agent.send(test_method)).to eq test_result
         end
       end
     end
   end
 
-  describe 'Browserino::Agent' do
+  describe 'Browserino::Agent defined names' do
+    ua    = browsers.sample[:user_agent]
+    agent = Browserino.parse ua
+
+    Browserino.names.each do |name|
+      it "defines agent.#{name}?" do
+        expect(agent.send("#{name}?")).to eq (agent.name == name)
+      end
+    end
+  end
+
+  describe 'Browserino::Agent defined types' do
     ua    = browsers.sample[:user_agent]
     agent = Browserino.parse ua
 
